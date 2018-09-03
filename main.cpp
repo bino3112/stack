f32562268d5eeb5366ac1ce5ff652e1119778a2c
bino3112 <bino3112@gmail.com>
2018-09-03 15:27:16 +0200

    TEST(?)

--- main.cpp
+++ main.cpp
@@ -8,18 +8,28 @@ int main() {
 	stack<int> s;
 	stack<int> s1(22);
 	stack<int> s2(3, 42);
-	std::cout << s2 << std::endl;
+	for (unsigned int i = s2.size(); i > 0; --i)
+	{
+		std::cout << "s2" << std::endl;
+		std::cout << s2.pop() << std::endl;
+	}
 	stack<int> s3(s2);
-	stack<int> s4(s2.begin(), s2.end());
-	stack<int> s5 = s2;
-	s5.bino(s1.begin(), s1.end());
-	s.clear();
-	s2.pop();
-	s2.push(22);
-	s2.size();
-	s.swap(s2);
-	s2.begin();
-	s2.end();
+	std::cout << s3.size() << std::endl;
+	for (unsigned int i = s3.size(); i > 0; --i)
+	{
+		std::cout << "s3" << std::endl;
+		std::cout << s3.pop() << std::endl;
+	}
+	// stack<int> s4(s2.begin(), s2.end());
+	// stack<int> s5 = s2;
+	// s5.bino(s1.begin(), s1.end());
+	// s.clear();
+	// s2.pop();
+	// s2.push(22);
+	// s2.size();
+	// s.swap(s2);
+	// s2.begin();
+	// s2.end();
 	for (stack<int>::const_iterator i = s2.begin(); i != s2.end(); ++i){
 		std::cout << "bino" << std::endl;
 	}
