e2cb275f441b3ab5e9223b12380e8287588de650
bino3112 <bino3112@gmail.com>
2018-09-06 17:32:33 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,21 +9,21 @@ int main() {
 	stack<int> s1(22);
 	stack<int> s2(4, 42);
 	stack<int> s3(s2);
-	// stack<int> s4(s2.begin(), s2.end()); // BAD_ALLOC
 	stack<int> s5 = s2;
-	// s5.bino(s1.begin(), s1.end());
+	s5.bino(s1.begin(), s1.end());
 	s2.clear();
 	s2.push(1);
 	s2.push(2);
 	s2.push(3);
 	s2.push(4);
 	s.swap(s3);
+	stack<int> s4(s2.begin(), s2.end());
 	std::cout << s2.size() << std::endl;
 	stack<int>::const_iterator a = s2.begin();
 	std::cout << *a << std::endl;
 	stack<int>::const_iterator b = s2.end();
 	std::cout << *b << std::endl;
-	for (stack<int>::const_iterator i = s2.begin(); i != s2.end(); ++i){
+	for (stack<int>::const_iterator i = s4.begin(); i != s4.end(); ++i){
 		stack<int>::const_iterator c = i;
 		std::cout << "nel for" << std::endl;
 		std::cout << *c << std::endl;
